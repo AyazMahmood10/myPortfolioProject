4c207b767c3aed201f6d8d0dd72bb97db6f549fd
ayazmahmood10 <ayazmahmood0015@gmail.com>
2021-08-13 17:23:06 +0530

    Profile picture size updated

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -17,7 +17,7 @@ const About = () => {
         <Row className="pt-3 pb-5 align-items-center">
           {/* Profile Picture */}
           <Col xs={12} md={6}>
-            <Row className="justify-content-center mb-2 mr-2">
+            <Row className="justify-content-center mr-2 mb-2 ml-2">
               <Image
                 className="profile justify-content-end"
                 alt="profile"
